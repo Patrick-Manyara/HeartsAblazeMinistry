a91a23a23df6c379b5f4cc7d7979f1503bb2f987
Patrick-Manyara <pmanyara97@gmail.com>
2021-03-05 17:12:48 +0300

    First push

---
+++ forms/contact.php
@@ -0,0 +1,52 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+
+  use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require '../includes/PHPMailer/vendor/autoload.php';
+
+
+
+if (isset($_POST['submit'])) {
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'pmanyara97@gmail.com';
+
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
+
+  $contact = new PHPMailer;
+ 
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'smtp.live.com',
+    'username' => 'vincentkusa170203@daystar.ac.ke',
+    'password' => 'Acs113114213',
+    'port' => '587'
+  );
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  $contact->send();
+
+}
+?>
